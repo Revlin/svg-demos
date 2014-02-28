cbc22accbdff1d0a5009aec8fdcaef6ce66ef733
Revlin John <stylogicalmaps@gmail.com>
2014-02-28 11:32:00 -0800

    route to svg-edit

--- uni-sol.pl
+++ uni-sol.pl
@@ -5,4 +5,16 @@ get '/svg-demos' => sub {
 	getReadme($self, 'svg-demos/README.md');
 };
 
+get '/svg-edit' => sub {
+	my $self = shift;
+	my $port  = $self->req->url->port;
+	my $base_url = '';
+    ( $base_url, $port ) = $self->req->url->base =~ /(.*[\w|\-|\.]+)(\:\d+)?/;
+	getFrame($self, 
+		$base_url, $port, 
+		'/svg-demos/svg-edit/editor/svg-editor.html', 
+		'Uni:Sol :: svg-edit'
+	);
+};
+
 1;
