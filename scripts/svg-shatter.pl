8517af5400af555c290c4c97e30e514755217755
Revlin John <stylogicalmaps@gmail.com>
2014-08-31 07:42:42 -0700

    update shatter-scene

--- scripts/svg-shatter.pl
+++ scripts/svg-shatter.pl
@@ -53,39 +53,66 @@ print $scene->desc, "\n";	# debug
 			height => "200"
 		);
 		
-		$scene->writer->startTag( 'g',
-			class => "shatter-layer0001",
-	   	  	transform => "translate(0,0)"
-		);
-			$scene->writer->startTag( 'clipPath', 
-				id => "char-test-shatter-layer0001-clip"
-			);
-				$scene->writer->emptyTag( 'path', 
-					d => "M 0 0 L 40 0 L 40 40 L 0 40 Z",
+		my( $swidth, $sheight, $rows ) = ( 200, 200, 10 );
+		
+		for( my $r=0; $r<$rows; $r++ ) {
+			# Create brick pattern with alternating width
+			my $h = int( $sheight / $rows );
+			my $y = $r * $h;
+			my $w = $h * (2 - $r%2);
+			my $x = 0;
+			my $blocks_per_row = int( $swidth/($h*1.5) );
+			$blocks_per_row++ if( ($blocks_per_row*($h*1.5)) < $swidth );
+				
+			for( my $s=0; $s<$blocks_per_row; $s++ ) {
+				my $layerId = $r * $blocks_per_row + $s + 1;
+				$layerId = ($layerId < 10)? "000$layerId":
+							($layerId < 100)? "00$layerId":
+							($layerId < 1000)? "0$layerId":
+							"$layerId";
+				print "$layerId, $s, $x, $w\n";
+
+				$scene->writer->startTag( 'g',
+					class => "shatter-layer$layerId",
 					transform => "translate(0,0)"
 				);
-			$scene->writer->endTag();
-			$scene->writer->emptyTag( 'rect',
-				transform => "translate(0,0)",
-				style => "fill:#ff0000; stroke:none; clip-path:url(#char-test-shatter-layer0001-clip);",
-				x => "0", 
-				y => "0",
-				width => "200",
-				height => "200"
-			);
-			$scene->writer->emptyTag( 'use',
-				transform => "translate(0,0)",
-				'xlink:href' => "#char-test-shatter-img",
-				style => "clip-path:url(#char-test-shatter-layer0001-clip);",
-				x => "0", 
-				y => "0",
-				width => "200",
-				height => "200"
-			);
-			$scene->writer->emptyTag( 'path',
-				d => "M 0 0 L 40 0 L 40 40 L 0 40 Z" 
-			);
-		$scene->writer->endTag();
+					$scene->writer->startTag( 'clipPath', 
+						id => "char-test-shatter-layer$layerId-clip"
+					);
+						$scene->writer->emptyTag( 'path', 
+							d => "M $x $y L ".($x+$w)." $y L ".($x+$w)." ".($y+$h)." L $x ".($y+$h)." Z",
+							transform => "translate(0,0)"
+						);
+					$scene->writer->endTag();
+					$scene->writer->emptyTag( 'rect',
+						transform => "translate(0,0)",
+						style => "fill:#ff0000; stroke:none; clip-path:url(#char-test-shatter-layer$layerId-clip);",
+						x => "0", 
+						y => "0",
+						width => "$swidth",
+						height => "$sheight"
+					);
+					$scene->writer->emptyTag( 'use',
+						transform => "translate(0,0)",
+						'xlink:href' => "#char-test-shatter-img",
+						style => "clip-path:url(#char-test-shatter-layer$layerId-clip);",
+						x => "0", 
+						y => "0",
+						width => "$swidth",
+						height => "$sheight"
+					);
+					$scene->writer->emptyTag( 'path',
+						#style => "stroke:none;",
+						d => "M $x $y L ".($x+$w)." $y L ".($x+$w)." ".($y+$h)." L $x ".($y+$h)." Z" 
+					);
+				$scene->writer->endTag();
+				
+				$x = $x + $w;
+				$w = (($r%2) < 1)? 
+						$h * ($s%2 + 1):
+						$h * (2 - $s%2);
+			}
+		}
 
 	$scene->writer->endTag();
 
