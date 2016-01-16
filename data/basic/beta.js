fec9f124220f57ba12822c7b19c268ca6ad56131
John <john@real-currents.com>
2016-01-15 20:42:52 -0800

    beta with raphael

--- data/basic/beta.js
+++ data/basic/beta.js
@@ -2,35 +2,38 @@
  * This is a Raphael canvas with a viewport 
  * and some basic content
  */
-var betaPaper = Raphael('beta', 640, 360);
+var betaPaper = Raphael('beta', 640, 360),
+    M='M', m='m', 
+    L='L', l='l', 
+    H='H', h='h', 
+    V='V', v='v',
+    Z='Z', z='z';
 
 betaPaper.canvas.attributes.style.value += "background: #FFF;";
 
-//<line x1="0" y1="180" x2="640" y2="180" />
+betaPaper.path([ M, 0, 180, L, 640, 180 ]);
 
-//<rect x="0" y="0" width="144" height="144" />
 betaPaper.rect(0, 0, 144, 144);
-//<rect x="0" y="0" width="72" height="72" />
 betaPaper.rect(0, 0, 72, 72);
 
-//<rect x="0" y="0" width="640" height="360" rx="72" ry="72" />
 betaPaper.rect(0, 0, 640, 360, 72);
 
-//<circle cx="480" cy="90" r="30" />
 betaPaper.circle(480, 90, 30);
 
-//<ellipse cx="480" cy="90" rx="90" ry="60" />
 betaPaper.ellipse(480, 90, 90, 60);
-//<ellipse cx="480" cy="270" rx="90" ry="60" />
 betaPaper.ellipse(480, 270, 90, 60);
 
-//<polygon points=" 135, 237.5
-//                137.9, 246.1
-//                146.9, 246.1
-//                139.7, 251.5
-//                142.3, 260.1
-//                135, 255
-//                127.7, 260.1
-//                130.3, 251.5
-//                123.1, 246.1
-//                132.1, 246.1" />
+betaPaper.path([ 
+    M, 135, 237.5,
+    L, 137.9, 246.1,
+    L, 146.9, 246.1,
+    L, 139.7, 251.5,
+    L, 142.3, 260.1,
+    L, 135, 255,
+    L, 127.7, 260.1,
+    L, 130.3, 251.5,
+    L, 123.1, 246.1,
+    L, 132.1, 246.1,
+    Z
+])
+    .attr({ fill: "red"  });
